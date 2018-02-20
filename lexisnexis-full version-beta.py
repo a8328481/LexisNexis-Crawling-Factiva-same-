670c034704805c60feccebd5338fc196066caf2b
Long Huang <a8328481@126.com>
2018-02-20 17:21:28 -0500

    Update lexisnexis-full version-beta.py

--- lexisnexis-full version-beta.py
+++ lexisnexis-full version-beta.py
@@ -36,34 +36,17 @@ class LexisNexis():
 
     def search(self):
         try:
-            self.driver.switch_to.frame(self.driver.find_element_by_tag_name("iframe"))
-            search_frame = self.driver.find_element_by_xpath("//input[@id = 'terms']")
-            search_frame.send_keys(self.key)
-            self.driver.find_element_by_xpath("//div[@id='advHeader']//img").click()
-            WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, "State and Federal Cases"))).click()
-            self.driver.find_element_by_id("Law Reviews").click()
-            self.driver.find_element_by_id("Company Profiles").click()
-            self.driver.find_element_by_id("txtFrmDate").send_keys(self.start_date)
-            self.driver.find_element_by_id("txtToDate").send_keys(self.end_date)
-            self.driver.find_element_by_xpath("//input[@id='OkButt']").click()
-            search_button = self.driver.find_element_by_xpath("//input[@id='srchButt']")
-            search_button.click()
+            *********
+            DEALING WITH FRAME QUESTION , EMAIL: lhuang54@fordham.edu
+            *********
         except:
             pass
 
     def predownload_action(self):
         try:
-            self.driver.switch_to.default_content()
-            self.driver.switch_to.frame(WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, "mainFrame"))))
-            self.driver.switch_to.frame(self.driver.find_element_by_xpath("//frame[@title='Results Content Frame']"))
-            Select(self.driver.find_element_by_xpath("//select[@class='goog-te-combo']")).select_by_value('en')
-            a_tag = self.driver.find_element_by_xpath("//tbody/tr[@class='noshaderow1st']/td/a")
-            first_url = a_tag.get_attribute('href')
-            self.driver.switch_to.default_content()
-            self.driver.switch_to.frame(self.driver.find_element_by_id("mainFrame"))
-            self.driver.switch_to.frame(self.driver.find_element_by_xpath("//frame[@title='Results Navigation Frame']"))
-            self.Count_of_article = self.driver.find_elements_by_tag_name("strong")[1].text
-            print self.Count_of_article
+            *********
+            DEALING WITH FRAME QUESTION , EMAIL: lhuang54@fordham.edu
+            *********
         except:
             pass
 
@@ -75,47 +58,15 @@ class LexisNexis():
             sleep(3)
             a = self.driver.page_source
             print a
-            download_page = self.driver.window_handles[1]
-            original_page = self.driver.window_handles[0]
-            self.driver.switch_to.window(download_page)
-            select_button = self.driver.find_element_by_id('sel')
-            select_button.click()
-            if int(self.Count_of_article)>int(500):
-                select_doc = self.driver.find_element_by_id('rangetextbox')
-                select_doc.send_keys('1-500')
-                format= Select(self.driver.find_element_by_id('delFmt'))
-                format.select_by_value('QDS_EF_GENERICTYPE')
-                self.driver.find_element_by_id('briefNote').send_keys(self.key)
-                download_button2 = self.driver.find_element_by_xpath("//img[@title='Download']")
-                download_button2.click()
-                download_link = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, "//p/a")))
-                download_link.click()
-                #back to original page
-
-                self.driver.switch_to.window(original_page)
-                self.driver.switch_to.default_content()
-                self.driver.switch_to.frame(self.driver.find_element_by_id("mainFrame"))
-                self.driver.switch_to.frame(self.driver.find_element_by_xpath("//frame[@title='Results Navigation Frame']"))
-                download_button = self.driver.find_element_by_xpath("//img[@title = 'Download Documents']")
-                download_button.click()
-                download_page = self.driver.window_handles[1]
-                self.driver.switch_to.window(download_page)
-                select_button = self.driver.find_element_by_id('sel')
-                select_button.click()
-                select_doc = self.driver.find_element_by_id('rangetextbox')
-                if int(self.Count_of_article) < int(1000):
-                    select_doc.send_keys('501-'+self.Count_of_article)
-                else:
-                    select_doc.send_keys('501-999')
-                format = Select(self.driver.find_element_by_id('delFmt'))
-                format.select_by_value('QDS_EF_GENERICTYPE')
-                self.driver.find_element_by_id('briefNote').send_keys(self.key)
-                download_button2 = self.driver.find_element_by_xpath("//img[@title='Download']")
-                download_button2.click()
-                download_link = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, "//p/a")))
-                download_link.click()
-                self.driver.switch_to.window(original_page)
-                self.driver.get('http://www.lexisnexis.com.avoserv2.library.fordham.edu/hottopics/lnacademic/')
+            
+            
+            
+            *********
+            DEALING WITH pop-up WINDOW QUESTION , EMAIL: lhuang54@fordham.edu
+            *********
+            
+            
+            
             else:
                 select_doc = self.driver.find_element_by_id('rangetextbox')
                 select_doc.send_keys('1-'+self.Count_of_article)
@@ -126,8 +77,11 @@ class LexisNexis():
                 download_button2.click()
                 download_link = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, "//p/a")))
                 download_link.click()
-                self.driver.switch_to.window(original_page)
-                self.driver.get('http://www.lexisnexis.com.avoserv2.library.fordham.edu/hottopics/lnacademic/')
+                
+                
+                *********
+                DEALING WITH pop-up WINDOW QUESTION , EMAIL: lhuang54@fordham.edu
+                *********
         except:
             pass
 
